9e6bd79f1ed902e4222344ad54595095c54d8b30
Dimitri Cunning <dccunning@gmail.com>
2025-04-10 21:19:30 -0700

    redefine new topics for producers/consumers

---
+++ trading_data/streaming/kafka/consumers/binance_api_all_price.py
@@ -0,0 +1,58 @@
+import os
+import json
+import time
+import logging
+from kafka import KafkaConsumer
+from clients.database import Database
+
+TOPIC = 'binance-api-all-price'
+GROUP = 'binance-api'
+BATCH_INTERVAL_SECONDS = 10.0
+
+consumer = KafkaConsumer(
+    TOPIC,
+    group_id=GROUP,
+    bootstrap_servers=os.getenv("BOOTSTRAP_SERVER"),
+    value_deserializer=lambda v: json.loads(v.decode('utf-8'))
+)
+logging.basicConfig(level=logging.INFO, format="%(asctime)s - %(filename)s - %(levelname)s: %(message)s")
+db = Database(host='192.168.1.67')
+
+insert_query = """
+INSERT INTO crypto.binance_api_all_price 
+(symbol, time, price)
+VALUES %s
+ON CONFLICT (symbol, time) DO NOTHING;
+"""
+create_table = """
+CREATE TABLE crypto.binance_api_all_price (
+    symbol TEXT NOT NULL,
+    time BIGINT NOT NULL,
+    price NUMERIC,
+    UNIQUE (symbol, time)
+);
+"""
+
+last_batch_time = time.time()
+buffer = []
+
+for message in consumer:
+    data = message.value
+    consumed_time = time.time() * 1000
+
+    row = (
+        data.get('symbol'),
+        data.get('time'),
+        data.get('price')
+    )
+    buffer.append(row)
+
+    if time.time() - last_batch_time >= BATCH_INTERVAL_SECONDS:
+        if buffer:
+            try:
+                db.run_query(insert_query, buffer)
+            except Exception as e:
+                logging.warning(f"{TOPIC}: Insert query failed: {e}")
+            logging.log(logging.INFO, f"{TOPIC}: Inserted {len(buffer)} rows")
+            buffer = []
+        last_batch_time = time.time()
---
+++ trading_data/streaming/kafka/producers/binance_api_all_book_ticker.py
@@ -0,0 +1,46 @@
+import os
+import json
+import logging
+import asyncio
+from aiokafka import AIOKafkaProducer
+from utils.kafka_utils.binance_api import producer_stream_api_book_price
+
+TOPIC = 'binance-api-all-book-ticker'
+FREQUENCY = 1.0
+KEY = 'symbol'
+URL = "https://fapi.binance.com/fapi/v1/ticker/bookTicker"
+
+"""
+kafka-topics.sh --create \
+  --bootstrap-server kafka:9092 \
+  --topic binance-api-all-book-ticker \
+  --partitions 12 \
+  --replication-factor 1
+
+kafka-topics.sh --bootstrap-server kafka:9092 --delete --topic binance-api-all-book-ticker
+"""
+
+logging.basicConfig(level=logging.INFO, format="%(asctime)s - %(filename)s - %(levelname)s: %(message)s")
+
+
+async def main():
+    producer = AIOKafkaProducer(
+        bootstrap_servers=os.getenv("BOOTSTRAP_SERVER"),
+        value_serializer=lambda v: json.dumps(v).encode("utf-8"),
+        key_serializer=lambda k: k.encode("utf-8")
+    )
+    await producer.start()
+    logging.log(logging.INFO, f"{TOPIC}: Started streaming")
+    try:
+        await producer_stream_api_book_price(
+            producer=producer,
+            topic=TOPIC,
+            key=KEY,
+            url=URL,
+            frequency=FREQUENCY
+        )
+    finally:
+        await producer.stop()
+
+if __name__ == "__main__":
+    asyncio.run(main())
